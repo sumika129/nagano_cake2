39bcf79a9e09ac19de9879dae11c59e2b0515563
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-04 14:57:30 +0000

    add admin/genre

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :customers, skip:[passwords], controllers:{
+  devise_for :customers, skip:[:passwords], controllers:{
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    resources :genres, only: [:index, :edit]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
