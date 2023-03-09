06b2c104176f4cbc405788b5c2c1ea61ffd61c48
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-09 14:58:22 +0000

    update admin/item/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,15 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render:index
+    end
   end
 
   def index
@@ -13,7 +20,12 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
-  
+
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active)
   end
 end
