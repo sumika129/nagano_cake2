379be3fc4ad0a056daca0f32692eadcbef265547
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-14 14:55:08 +0000

    update admin/item/show

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage2.png')
+      image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def with_tax_price
+    (price*1.1).floor
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :items, only: [:new, :index, :create, :show, :edit, :update]
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
   end
 
 
