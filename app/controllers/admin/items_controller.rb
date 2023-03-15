a6b739e4c0d4b95e9069cb01edf2123684c60f81
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-15 15:19:04 +0000

    update admin/item/edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,7 +6,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_item_path(@item.id)
     else
       render:index
     end
@@ -21,9 +21,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path(@item.id)
+    else
+      render:edit
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/noimage2.png')
       image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
