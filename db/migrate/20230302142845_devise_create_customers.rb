9d3705df45637681d8407213f128df2d11258954
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-18 07:21:05 +0000

    update admin/customer/index

--- db/migrate/20230302142845_devise_create_customers.rb
+++ db/migrate/20230302142845_devise_create_customers.rb
@@ -33,6 +33,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
 
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_deleted, default: false
+
       t.timestamps null: false
     end
 
