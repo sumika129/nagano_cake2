93131624b88c2207a3831f5aa932ebb635605b7f
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-20 14:41:10 +0000

    add admin/home

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
+    root to: "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
