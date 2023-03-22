d1e47026c2aee1ed5d1600eac4a9343b3cf06785
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-22 14:14:44 +0000

    add admin/order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_144701) do
+ActiveRecord::Schema.define(version: 2023_03_22_140344) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -86,6 +86,19 @@ ActiveRecord::Schema.define(version: 2023_03_21_144701) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "postal_code"
+    t.string "address"
+    t.string "name"
+    t.integer "status"
+    t.integer "billing_amount"
+    t.integer "postage"
+    t.integer "payment_method"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
