0889d779fb1f3a7a6cd40b6a6850ad9e715ced38
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-03-30 14:18:33 +0000

    update public/customer/edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,9 +4,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
-    
-  end  
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(current_customer)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
 end
