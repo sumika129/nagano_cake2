b2cb7cefb1227007ec1cddee9e0df875f2701209
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-03 15:04:56 +0000

    update address/index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,29 +1,32 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
+    @addresses = current_customer.addresses
   end
-  
+
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to public_addresses_path
     else
+      @addresses = current_customer.addresses
       render:index
-    end  
+    end
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
-  end  
+  end
 end
