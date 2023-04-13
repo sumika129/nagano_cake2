a25d76d806adeeaaed8192d02e87598383e0fe95
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-13 14:35:05 +0000

    update cart_item/controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
-
+    @cart_item = CartItem.all
+    @total = 0
   end
 
   def update
@@ -8,6 +9,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.save
+    redirect_to public_cart_items_path
+  end
 
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id)
   end
 end
