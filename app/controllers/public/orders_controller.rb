05685d61616cabdf42cfaefff25ce3b82c058912
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-20 14:53:30 +0000

    update address/model

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,12 +5,17 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
-
+    @order = Order.new(order_params)
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :customer_id, :billing_amount, :postage)
   end
 end
