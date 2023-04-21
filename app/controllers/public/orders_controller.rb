ff6a86c2064317b1ec7e5ac0f6744fab0c551273
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-21 12:13:59 +0000

    update public/order/new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,25 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
-    @address = current_customer.address
+    @address = current_customer.addresses
   end
 
   def comfirm
     @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    if params[:order][:select_address] == "a"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "b"
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:select_address] == "c"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
   end
 
   def index
