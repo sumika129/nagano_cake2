c77ae602f32fc47d70a84762dc5aa78a7545282c
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-28 15:05:27 +0000

    update order_detail/model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_details
 
   def address_display
     '〒' + postal_code + ' ' + address + ' ' + name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_08_064951) do
+ActiveRecord::Schema.define(version: 2023_04_28_145827) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -111,6 +111,16 @@ ActiveRecord::Schema.define(version: 2023_04_08_064951) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
