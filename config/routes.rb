8920b4a777f9dcde77bed7b79317973684498d45
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-04-30 14:57:43 +0000

    update order/comfirm

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show]
+    resources :order_details, only: [:update]
   end
 
   namespace :public do
